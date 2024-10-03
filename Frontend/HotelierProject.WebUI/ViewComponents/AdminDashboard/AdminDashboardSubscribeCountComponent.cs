facd144c27bd2fe70116f8d79ef3f7a95d275c0c
halilselvitopu <halilselvitopu@gmail.com>
2024-10-03 18:21:55 +0300

    Instagram API for followers and following count

--- Frontend/HotelierProject.WebUI/ViewComponents/AdminDashboard/AdminDashboardSubscribeCountComponent.cs
+++ Frontend/HotelierProject.WebUI/ViewComponents/AdminDashboard/AdminDashboardSubscribeCountComponent.cs
@@ -1,12 +1,39 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using System.Net.Http;
+using System;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+using HotelierProject.WebUI.Dtos.FollowersDto;
+using Newtonsoft.Json;
 
 namespace HotelierProject.WebUI.ViewComponents.AdminDashboard
 {
     public class AdminDashboardSubscribeCountComponent : ViewComponent
     {
-        public IViewComponentResult Invoke()
+        public async Task<IViewComponentResult> InvokeAsync()
         {
-            return View();
+            
+            var client = new HttpClient();
+            var request = new HttpRequestMessage
+            {
+                Method = HttpMethod.Get,
+                RequestUri = new Uri("https://instagram-profile1.p.rapidapi.com/getprofileinfo/microsoft"),
+                Headers =
+    {
+        { "x-rapidapi-key", "4034e659c0msh83d8f6ae7d77004p134fffjsnf5fb44dbe656" },
+        { "x-rapidapi-host", "instagram-profile1.p.rapidapi.com" },
+    },
+            };
+            using (var response = await client.SendAsync(request))
+            {
+                response.EnsureSuccessStatusCode();
+                var body = await response.Content.ReadAsStringAsync();
+                ResultInstagramFollowersDto resultInstagramFollowersDto = JsonConvert.DeserializeObject<ResultInstagramFollowersDto>(body);
+                ViewBag.followersCount = resultInstagramFollowersDto.followers;
+                ViewBag.followingCount = resultInstagramFollowersDto.following;
+                return View(resultInstagramFollowersDto);
+            }
+           
         }
     }
 }
